2a5ddb23cc35b8cbcf1faecaa8382a8d4c7d40db
Kamil <kaqil91@gmail.com>
2017-03-31 03:23:18 -0700

    some javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+// create an array of 6 letter words
+// these will be used as the words
+// to guess in the game
+  var possibleWords = ["hacker","jazzed","pizzas","pajama","jicama","sphynx","jigsaw"];
+// User starts with 6 chances to guess a 6 letter word
+  var numberOfGuesses = 6;
+// add click event listener to resetButton
+  var resetButton = document.getElementById('reset');
+
+// pick random word to guess
+  var wordToGuess;
+  pickWord();
+  console.log(wordToGuess);
+// display to game board
+  displayToGameBoard(wordToGuess);
+//create a game object
+var game = {
+  letterOne:document.getElementById('letterOne'),
+  letterTwo:document.getElementById('letterTwo'),
+  letterThree:document.getElementById('letterThree'),
+  letterFour:document.getElementById('letterFour'),
+  letterFive:document.getElementById('letterFive'),
+  letterSix:document.getElementById('letterSix'),
+};
+
+// this is what happens when
+// the user keys something
+  document.onkeyup = function(event){
+    var userEntry = event.key;
+    console.log(userEntry);
+  // while number of guesses is not
+  // zero
+  console.log(numberOfGuesses);
+}
+
+
+function pickWord(){
+  var arrayIndex = Math.floor(Math.random()*7);
+  // console.log(arrayIndex);
+  wordToGuess = possibleWords[arrayIndex];
+}
+function displayToGameBoard(wordToGuess){
+  // split the word into an array
+  // and display on screen
+  // and then add hide class 
+  wordToGuess = wordToGuess.split("");
+  document.getElementById('letterOne').innerHTML = wordToGuess[0];
+  document.getElementById('letterTwo').innerHTML = wordToGuess[1];
+  document.getElementById('letterThree').innerHTML = wordToGuess[2];
+  document.getElementById('letterFour').innerHTML = wordToGuess[3];
+  document.getElementById('letterFive').innerHTML = wordToGuess[4];
+  document.getElementById('letterSix').innerHTML = wordToGuess[5];
+
+  document.getElementById('letterOne').className+=" hide";
+  document.getElementById('letterTwo').className+=" hide";
+  document.getElementById('letterThree').className+=" hide";
+  document.getElementById('letterFour').className+=" hide";
+  document.getElementById('letterFive').className+=" hide";
+  document.getElementById('letterSix').className+=" hide";
+}
