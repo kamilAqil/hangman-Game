d0fec8953eda3b541e297471b986f42f061895ae
Kamil <kaqil91@gmail.com>
2017-03-31 16:34:39 -0700

    win and lose works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@
 // these will be used as the words
 // to guess in the game
   var possibleWords = ["hacker","jazzed","pizzas","pajama","jicama","sphynx","jigsaw"];
+  var lettersUsed = [];
 // User starts with 6 chances to guess a 6 letter word
   var numberOfGuesses = 6;
   document.getElementById('triesLeft').innerHTML = numberOfGuesses;
@@ -14,34 +15,65 @@
   console.log(wordToGuess);
 // display to game board
   displayToGameBoard(wordToGuess);
-//create a game object
-var game = {
-  letterOne:document.getElementById('letterOne'),
-  letterTwo:document.getElementById('letterTwo'),
-  letterThree:document.getElementById('letterThree'),
-  letterFour:document.getElementById('letterFour'),
-  letterFive:document.getElementById('letterFive'),
-  letterSix:document.getElementById('letterSix'),
+//create a gameBoard object
+var gameBoard = {
+  letters:{
+    letterOne:document.getElementById('letterOne'),
+    letterTwo:document.getElementById('letterTwo'),
+    letterThree:document.getElementById('letterThree'),
+    letterFour:document.getElementById('letterFour'),
+    letterFive:document.getElementById('letterFive'),
+    letterSix:document.getElementById('letterSix')
+  }
 };
-
-// run the game numberOfGuesses times
+var lettersToGuessArray = [gameBoard["letters"]["letterOne"].innerHTML,gameBoard["letters"]["letterTwo"].innerHTML,
+                          gameBoard["letters"]["letterThree"].innerHTML,gameBoard["letters"]["letterFour"].innerHTML,
+                          gameBoard["letters"]["letterFive"].innerHTML,gameBoard["letters"]["letterSix"].innerHTML,];
+console.log(lettersToGuessArray);
+// if numberOfGuesses is not 0
+// allow the user to play the round
 if(numberOfGuesses !== 0){
-  // this is what happens when
-  // the user keys something
+    // userEntry is used to store the
+    // value entered by the user
     document.onkeyup = function(event){
       var userEntry = event.key;
+      if(userEntry!=="Meta"){
+        lettersUsed.push(userEntry);
+      }
+      console.log(lettersUsed);
+      document.getElementById('lettersUsedParagraph').innerHTML = lettersUsed;
       console.log(userEntry);
+      // for every letter on the game board letters object
+      // if the userEntry is == to the letter
+      // on the gameBoard remove the hide class
+      // from the piece on the gameBoard also
+      // remove that letter from lettersToGuessArray
       var x;
-      for (x in game ){
-        if(game[x].innerHTML == userEntry){
-          game[x].classList.remove('hide');
+      for (x in gameBoard.letters ){
+        // if the userEntry exits in the gameBoard
+        if(gameBoard.letters[x].innerHTML == userEntry){
+          // remove the hide class on gameBoardLetter
+          gameBoard.letters[x].classList.remove('hide');
+          // get indexToDelete and delete letter from lettersToGuessArray
+          var indexToDelete = lettersToGuessArray.indexOf(userEntry);
+          lettersToGuessArray.splice(indexToDelete,1);
+          // once deleted if lettersToGuessArray is empty
+          // run the win function
+          if(lettersToGuessArray.length == 0){
+            win();
+          }
         }
       }
-      if(userEntry in game == false && userEntry!=="Meta"){
+      console.log(lettersToGuessArray);
+      if(userEntry in gameBoard == false && userEntry!=="Meta"){
         numberOfGuesses--;
         updateNumberOfGuesses();
+        // if number of Guesses = 0
+        // run the lose function
+        if (numberOfGuesses==0){
+          lose();
+        }
       }
-
   };
 }
 
@@ -71,4 +103,11 @@ function displayToGameBoard(wordToGuess){
 }
 function updateNumberOfGuesses(){
   document.getElementById('triesLeft').innerHTML = numberOfGuesses;
+}
+
+function win(){
+  console.log('you win!');
+}
+function lose(){
+  console.log('you lose!');
 }
